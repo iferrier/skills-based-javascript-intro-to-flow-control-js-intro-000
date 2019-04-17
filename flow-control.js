6cfe635b26eb28cae4889f9b4c6feb4e6ff56db6
Isabelle Ferrier <ferrierisabelle@gmail.com>
2019-04-17 11:11:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -25,7 +25,17 @@ return (age >= 13 && age <= 19 ? 'You are a teenager' : 'You are not a teenager'
 }
 
 function switchAge(age) {
-case (age >= 13 && age <= 19) : return(you are a teenager)
+case 13 : return(You are a teenager);
+case 14 : return(You are a teenager);
+case 15 : return(You are a teenager);
+case 16 : return(You are a teenager);
+case 17 : return(You are a teenager);
+case 18 : return(You are a teenager);
+case 19 : return(You are a teenager);
+  break;return(You have an age);
+  break;
+  break;
+  break;
 break;
 default: return(You have an age)
   break;
